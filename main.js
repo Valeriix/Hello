1ee02afcc403f8df11ef06d8ce20c7fce575cec1
Valeriix <124144056+Valeriix@users.noreply.github.com>
2023-07-21 03:11:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+//This is a comment in JavaScript
+
+//Here we bind the click event of element with ID "click-button" to our function clickButton() to display an alert
+document.getElementById("click-button").addEventListener("click", clickButton);
+function clickButton() { 
+  alert("nice click!"); 
+} 
